d1ed6e3a571dc218f09c7690d73fc7efe24740c6
Clovis Gilles <clovis.gilles@epitech.eu>
2023-03-08 12:57:20 +0000

    imporvement in the assistant GPS but still some bug

--- Entities/Assistant.java
+++ Entities/Assistant.java
@@ -23,6 +23,7 @@ public class Assistant extends SynchronizedThread {
     private int assistantMaxTimeBeforeBreak;
     private String currentPosition = "delivery";
     private int waitingTicks = 0;
+    private ArrayList<Section> destinationList;
 
     private boolean printEndAction = false;
     private String msgEndAction;
@@ -40,13 +41,72 @@ public class Assistant extends SynchronizedThread {
         Logger.writeLog("T = " + currentTick + " | A new assistant is created");
     }
 
+    private void GPSSetUp() {
+        this.destinationList = new ArrayList<Section>();
+
+        for (Section currentSection : this.sectionList) {
+            if (searchBookForSection(currentSection.getName())) {
+                currentSection.getNbWaitingCustomer();
+                if (this.destinationList.size() == 0) {
+                    this.destinationList.add(currentSection);
+                } else {
+                    int index = 0;
+                    Boolean isAdd = false;
+                    for (; index < this.destinationList.size() && isAdd == false; index++) {
+                        if (this.destinationList.get(index).getNbWaitingCustomer() < currentSection.getNbWaitingCustomer()) {
+                            this.destinationList.add(index, currentSection);
+                            isAdd = true;
+                        }
+                    }
+                    if (isAdd == false) {
+                        this.destinationList.add(currentSection);
+                    }
+                }
+            }
+        }
+        for (Section test: this.destinationList) {
+            System.out.println(test.getName());
+        }
+    }
+
+    private void chooseNextDestination(Boolean availableSection) {
+        if (availableSection == false) {//Don't work, if the delivery is full, he leave and go back
+            if (this.currentBookList.size() > 1 && this.destinationList.size() > 1) {
+                this.destinationList.remove(0);
+                moveAction(this.destinationList.get(0).getName());
+                this.destinationList.remove(0);
+            } else if (this.currentBookList.size() > 1) {
+                moveAction(this.currentBookList.get(1).getSection());
+            } else {
+                moveAction("delivery");
+            }
+        }
+        if (this.currentBookList.size() > 0 && this.destinationList.size() > 0) {
+            moveAction(this.destinationList.get(0).getName());
+            this.destinationList.remove(0);
+        } else if (this.currentBookList.size() > 0) {
+            moveAction(this.currentBookList.get(0).getSection());
+        } else {
+            moveAction("delivery");
+        }
+    }
+
     private Section searchCurrentSection(String targetSectionName) {
         int index = 0;
         for (; this.sectionList.get(index).getName() == targetSectionName; index++);
         return sectionList.get(index);
     }
 
-    private Book searchBookForSection(String targetSectionName) {
+    private Boolean searchBookForSection(String targetSectionName) {
+        for (int index = 0; index < this.currentBookList.size(); index++) {
+            if (this.currentBookList.get(index).getSection() == targetSectionName) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Book getBookForSection(String targetSectionName) {
         if (this.currentBookList.size() == 0)
             return null;
         for (int index = 0; index < this.currentBookList.size(); index++) {
@@ -85,37 +145,32 @@ public class Assistant extends SynchronizedThread {
                 }
                 if (this.currentBookList.size() > 0) {
                     Logger.writeLog("T = " + this.scheduler.getTickNumber() + " | A assistant has took " + comp + " book(s) from the delivery box.");
-                    moveAction(this.currentBookList.get(0).getSection());
+                    GPSSetUp();
+                    chooseNextDestination(true);
                 }
             } else {
                 // Section behaviour
                 Section currentSection = searchCurrentSection(this.currentPosition);
 
                 if ((currentSection.getNbCurrentBook() + 1) <= currentSection.getNbMaxBook()) {
-                    Book currentBook = searchBookForSection(this.currentPosition);
+                    Book currentBook = getBookForSection(this.currentPosition);
                     if (currentBook != null) {
                         currentSection.addBook(currentBook);
                         this.waitingTicks = this.assistantTimeInsertBookIntoSection - 1;
 
-                        Logger.writeLog("T = " + this.scheduler.getTickNumber() + " | A assistant begins to put a book in the " + this.currentPosition + " section");
+                        String logComplement = "is putting";
                         if (this.waitingTicks > 0) {
                             this.printEndAction = true;
                             this.msgEndAction = "A assistant has finished to put a book in the " + this.currentPosition + " section";
+                            logComplement = "begins to put";
                         }
+                        Logger.writeLog("T = " + this.scheduler.getTickNumber() + " | A assistant " + logComplement + " a book in the " + this.currentPosition + " section");
 
                     } else {
-                        if (this.currentBookList.size() == 0) {
-                            moveAction("delivery");
-                        } else {
-                            moveAction(this.currentBookList.get(0).getSection());
-                        }
+                        chooseNextDestination(true);
                     }
                 } else {
-                    if (this.currentBookList.size() == 0) {
-                        moveAction("delivery");
-                    } else {
-                        moveAction(this.currentBookList.get(0).getSection());
-                    }
+                    chooseNextDestination(false);
                 }
             }
         }
